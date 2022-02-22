8111fe99b5abf1662c28fd233dfbe95bc651d507
Achintha <achinthabandara1@gmail.com>
2022-02-22 23:19:33 +0530

    port

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.post('/api', (req, res) => {
     });
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT||port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
